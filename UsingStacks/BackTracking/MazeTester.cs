25d7fa21fd97f5b484f6990ff82cba532ce07a02
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-09-12 08:06:44 +0000

    Add project files.

---
+++ UsingStacks/BackTracking/MazeTester.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingStacks.BackTracking
+{
+    internal class MazeTester
+    {
+        static int[,] maze = new int[,] {
+                { 1, 1,0,0 },
+                { 1, 0,0,0 },
+                { 1, 1,1,0 },
+                { 0,1,1,1 } };
+        static int[,] path = new int[maze.GetLength(0), maze.GetLength(1)];
+
+        static void Main(string[] args)
+        {
+            FindPath(0, 0, maze.GetLength(0));
+            for (int i = 0; i < maze.GetLength(0); i++)
+            {
+                Console.WriteLine( "");
+                for (int j = 0; j < maze.GetLength(1) ; j++)
+                {
+                    Console.Write(path[i, j]);
+                }
+            }
+            Console.ReadLine(  );
+        }
+
+        public static int FindPath(int row, int col,int size) {
+            if (row==(size-1) && col== (size))
+            {
+                return 1;
+            }
+
+            if (maze[row,col]==1)
+            {
+                path[row,col] = 1;
+                if (FindPath(row,col+1,size)==1)return 1;
+                if(FindPath(row+1,col,size)==1)return 1;
+                path[row,col] = 0;
+            }
+            return 0;
+        }
+    }
+}
---
+++ UsingStacks/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace UsingStacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string words = "today is tuesday";
+            string results="";
+
+            for (int i = words.Length-1; i >= 0; i--)
+            {
+                results += words[i];
+            }
+            Console.WriteLine(results);
+
+            ReveseString(words);
+            Console.Read(  );
+        }
+
+        public static void ReveseString(string normalWord) {
+
+            Stack<char> mystack = new Stack<char>();
+
+            for (int i = 0; i<normalWord.Length; i++)
+            {
+                mystack.Push(normalWord[i]);
+            }
+            foreach (char c in mystack) {
+                Console.Write(c);
+            }
+        }
+    }
+}
